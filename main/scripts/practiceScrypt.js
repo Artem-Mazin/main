57268dd4d9d1291b9ae97da1f78f73a382033b66
Artem_Mazin <artem.mazin.js@gmail.com>
2022-10-03 16:41:34 +0300

    add animation first card

--- main/scripts/practiceScrypt.js
+++ main/scripts/practiceScrypt.js
@@ -1,3 +1,4 @@
+// карточки с фильмами
 class card {
   constructor(link, title, image, parentSelector) {
     this.link = link;
@@ -7,7 +8,7 @@ class card {
   }
 
   render() {
-    const element = document.createElement('div');
+    const element = document.createElement("div");
     element.innerHTML = `
     <div class="card">
     <a href='${this.link}' class="card__item" target="blank">
@@ -20,44 +21,59 @@ class card {
   }
 }
 new card(
-  '/TheInformer.html',
-  'The Informer',
-  'https://www.film.ru/sites/default/files/movies/posters/33019018-1084728.jpg',
-  '.content',
+  "/TheInformer.html",
+  "The Informer",
+  "https://www.film.ru/sites/default/files/movies/posters/33019018-1084728.jpg",
+  ".content"
 ).render();
 
 new card(
-  '/Leon.html',
-  'Leon',
-  'https://www.film.ru/sites/default/files/movies/posters/Leon-9.jpg',
-  '.content',
+  "/Leon.html",
+  "Leon",
+  "https://www.film.ru/sites/default/files/movies/posters/Leon-9.jpg",
+  ".content"
 ).render();
 
 new card(
-  '',
-  'Whiplash',
-  'https://www.film.ru/sites/default/files/movies/posters/3790998-1718031.jpeg',
-  '.content',
+  "",
+  "Whiplash",
+  "https://www.film.ru/sites/default/files/movies/posters/3790998-1718031.jpeg",
+  ".content"
 ).render();
 
 new card(
-  '',
-  'Interstellar',
-  'https://www.film.ru/sites/default/files/movies/posters/1703315-663706.jpg',
-  '.content',
+  "",
+  "Interstellar",
+  "https://www.film.ru/sites/default/files/movies/posters/1703315-663706.jpg",
+  ".content"
 ).render();
 
 new card(
-  '',
-  'The Pianist',
-  'https://www.film.ru/sites/default/files/movies/posters/1610623-621640.jpg',
-  '.content',
+  "",
+  "The Pianist",
+  "https://www.film.ru/sites/default/files/movies/posters/1610623-621640.jpg",
+  ".content"
 ).render();
 
-const clickHamb = document.querySelector('.hamb');
-const menu = document.querySelector('.menu');
-clickHamb.addEventListener("click", function() {
-menu.classList.toggle('_active');
+// открывание/закрывание меню
+const clickHamb = document.querySelector(".hamb");
+const menu = document.querySelector(".menu");
+clickHamb.addEventListener("click", function () {
+  menu.classList.toggle("_active");
+});
+clickHamb.addEventListener("mouseenter", function () {
+  menu.classList.toggle("_active");
+});
+menu.addEventListener("mouseleave", function () {
+  menu.classList.toggle("_active");
 });
 
-
+// подсветка карточек
+const cards = document.querySelector(".card");
+const card__image = document.querySelector(".card__image");
+cards.addEventListener("mouseenter", function () {
+  card__image.style.height = "21em";
+});
+cards.addEventListener("mouseleave", function () {
+  card__image.style.height = "";
+});
